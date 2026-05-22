974ef7d851f442d58e3e7cdc272decbf967c1763
kentliukx <kentliukx@163.com>
2026-05-22 21:47:58 +0800

    fix: model penetration on rough terrain

--- legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/envs/base/legged_robot_config.py
@@ -219,7 +219,7 @@ class LeggedRobotCfg(BaseConfig):
 
     class sim:
         dt =  0.005
-        substeps = 1
+        substeps = 4
         gravity = [0., 0. ,-9.81]  # [m/s^2]
         up_axis = 1  # 0 is y, 1 is z
 
@@ -229,9 +229,9 @@ class LeggedRobotCfg(BaseConfig):
             num_position_iterations = 4
             num_velocity_iterations = 0
             contact_offset = 0.01  # [m]
-            rest_offset = 0.0   # [m]
+            rest_offset = 0.001   # [m]
             bounce_threshold_velocity = 0.5 #0.5 [m/s]
-            max_depenetration_velocity = 1.0
+            max_depenetration_velocity = 10.0
             max_gpu_contact_pairs = 2**24 #2**24 -> needed for 8000 envs and more
             default_buffer_size_multiplier = 10
             contact_collection = 2 # 0: never, 1: last sub-step, 2: all sub-steps (default=2)
