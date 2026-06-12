86d3f3f143230836cfa449f280aa629e384fca92
kentliukx <kentliukx@163.com>
2026-06-12 17:04:20 +0800

    update: decrease history len

--- legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/envs/base/legged_robot_config.py
@@ -33,10 +33,10 @@ from .base_config import BaseConfig
 class LeggedRobotCfg(BaseConfig):
     class env:
         num_envs = 2048
-        num_observations = 4983
+        num_observations = 3303
         num_privileged_obs = None # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise 
         num_actions = 12
-        proprioception_history_len = 50
+        proprioception_history_len = 10
         debug_terrain_sampling = False
         env_spacing = 3.  # not used with heightfields/trimeshes 
         send_timeouts = True # send time out information to the algorithm
